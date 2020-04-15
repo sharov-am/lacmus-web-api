a6d130c39dc0f8b5a41c77a4f21e65cf9acd79c8
sharov_am <sharov_am@mail.ru>
2020-04-15 21:40:09 +0300

    1)clean-up; 2)preliminary data model; 3)small fixes;

---
+++ lacmus_WebApp/lacmus.WebApp/5.4.0/aspnet-core/src/lacmus.WebApp.Model/LacmusImage.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Domain.Entities.Auditing;
+
+namespace lacmus.WebApp.Model
+{
+    public class LacmusImage : FullAuditedEntity<long>
+    {
+        public string FileName { get; set; }
+
+        private string Hash { get; set; }
+
+        private ImageAnnotation Annotation { get; set; }
+
+    }
+}
